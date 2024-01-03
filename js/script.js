476b596de53dfa0d309fda7f92ca456374c02915
nurali-code <nur3.dav.97@gmail.com>
2024-01-03 14:11:23 +0500

    menu done

--- js/script.js
+++ js/script.js
@@ -1,41 +1,9 @@
-$(document).ready(function () {
-    $('.btn__menu').on('click', () => { $('body, .header nav, .btn__menu').toggleClass('active'); })
-    $('.dropdown-btn').on('click', function (e) { $(this).toggleClass('active').next('.dropdown-content').slideToggle(300); })
+var btnMenu = document.getElementById('btnMenu');
+var navbar = document.getElementById('navbar');
+var body = document.body;
 
-    $(document).on('click', function (e) {
-        if (!(($(e.target).parents('.navbar').length) ||
-            ($(e.target).parents('btn__menu').length) ||
-            ($(e.target).parents('modal-content').length) ||
-            ($(e.target).hasClass('btnPlay')) ||
-            ($(e.target).hasClass('btn__menu')) ||
-            ($(e.target).hasClass('navbar'))
-        )) {
-            $('#video_modal').fadeOut(300);
-            $('body, .header nav, .btn__menu').removeClass('active');
-        }
+btnMenu.addEventListener('click', () => {
+    [btnMenu, navbar, body].forEach(element => {
+        element.classList.toggle('active');
     });
-
-    $(document).on('click', '.btnPlay', function (e) {
-        $('#video_modal').fadeIn(300);
-    });
-
-    if (window.innerWidth >= 1000) {
-        $('.slider').slick({
-            arrows: true,
-            infinite: false,
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            responsive: [{ breakpoint: 1000, settings: "unslick" }]
-        });
-
-    }
-
-    $('.copyText').on('click', function () {
-        var dataToCopy = $(this).data('copy');
-        navigator.clipboard.writeText(dataToCopy)
-            .then(function () { alert('Промокод скопирован: ' + dataToCopy); })
-            .catch(function (err) { console.error('Не удалось скопировать промокод: ', err); });
-    })
-
 });
-
